13d445b36e829f9ee9230edd568d9fa541eda62f
Pulkit Gupta <pulkit.gupta@gojek.com>
2023-03-22 16:11:09 +0530

    Add CartItem domain model with it's unit test

---
+++ src/main/java/domain/CartItem.java
@@ -0,0 +1,47 @@
+package domain;
+
+import java.util.Objects;
+
+public class CartItem {
+  private final Product product;
+  private final int quantity;
+
+  public CartItem(final Product product, final int quantity) {
+    this.product = product;
+    this.quantity = quantity;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    CartItem cartItem = (CartItem) o;
+
+    if (this.getQuantity() != cartItem.getQuantity()) return false;
+    return Objects.equals(this.getProduct(), cartItem.getProduct());
+  }
+
+  @Override
+  public int hashCode() {
+    int result = this.getProduct() != null ? this.getProduct().hashCode() : 0;
+    result = 31 * result + this.getQuantity();
+    return result;
+  }
+
+  protected boolean hasSameProduct(Product product) {
+    return this.getProduct().equals(product);
+  }
+
+  protected CartItem addQuantityBy(int quantity) {
+    return new CartItem(this.getProduct(), this.getQuantity() + quantity);
+  }
+
+  protected Product getProduct() {
+    return product;
+  }
+
+  protected int getQuantity() {
+    return quantity;
+  }
+}
---
+++ src/test/java/domain/CartItemTest.java
@@ -0,0 +1,142 @@
+package domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+
+@ExtendWith(MockitoExtension.class)
+class CartItemTest {
+  private CartItem cartItem;
+  private Product product;
+
+  @Mock
+  private Product mockProduct;
+
+  @Test
+  @DisplayName("Test hasSameProduct should return true when same cheerios product is given")
+  void testHasSameProduct_ShouldReturnTrueWhenGivenSameProduct() {
+    //Given
+    product = new Product("cheerios", new BigDecimal("10"));
+    cartItem = new CartItem(product, 1);
+
+    //When
+    boolean result = cartItem.hasSameProduct(new Product("cheerios", new BigDecimal("10")));
+
+    //Then
+    Assertions.assertTrue(result);
+  }
+
+  @Test
+  @DisplayName("Test hasSameProduct should return false when different product is given")
+  void testHasSameProduct_ShouldReturnTrueWhenGivenDifferentProduct() {
+    //Given
+    product = new Product("cornflakes", new BigDecimal("10"));
+    cartItem = new CartItem(product, 1);
+
+    //When
+    boolean result = cartItem.hasSameProduct(new Product("cheerios", new BigDecimal("10")));
+
+    //Then
+    Assertions.assertFalse(result);
+  }
+
+  @Test
+  @DisplayName("Test getProduct should return the cornflakes product having 10.01 price present in the cart item")
+  void testGetProduct_ShouldReturnProductPresentInCartItem() {
+    //Given
+    product = new Product("cornflakes", new BigDecimal("10.01"));
+    cartItem = new CartItem(product, 2);
+
+    //When
+    Product actualProduct = cartItem.getProduct();
+
+    //Then
+    Assertions.assertEquals(product, actualProduct);
+  }
+
+  @Test
+  @DisplayName("Test getQuantity should return the correct quantity of cornflakes product in the cart item")
+  void testGetQuantity_ShouldReturnQuantityPresentInCartItem() {
+    //Given
+    product = new Product("cornflakes", new BigDecimal("10.01"));
+    int quantity = 2;
+    cartItem = new CartItem(product, quantity);
+
+    //When
+    int actualQuantity = cartItem.getQuantity();
+
+    //Then
+    Assertions.assertEquals(quantity, actualQuantity);
+  }
+
+  @Test
+  @DisplayName("Test equals for reflexive behaviour as per the equal's contract")
+  void testEqualsForReflexiveBehaviour_ShouldReturnTrueWhenGivenSameProduct() {
+    //Given
+    int quantity = 2;
+    product = new Product("cornflakes", new BigDecimal("10.01"));
+    cartItem = new CartItem(product, quantity);
+
+    //When
+    boolean result = cartItem.equals(cartItem);
+
+    //Then
+    Assertions.assertTrue(result);
+  }
+
+  @Test
+  @DisplayName("Test equals for symmetric behaviour as per the equal's contract")
+  void testEqualsForSymmetricBehaviour_ShouldReturnTrueWhenGivenSameProduct() {
+    //Given
+    product = new Product("cornflakes", new BigDecimal("10.01"));
+    cartItem = new CartItem(product, 2);
+    CartItem secondCartItem = new CartItem(new Product("cornflakes", new BigDecimal("10.01")), 2);
+
+    //When
+    boolean result = cartItem.equals(secondCartItem);
+    boolean anotherResult = secondCartItem.equals(cartItem);
+
+    //Then
+    Assertions.assertTrue(result);
+    Assertions.assertTrue(anotherResult);
+  }
+
+  @Test
+  @DisplayName("Test equals for transitive behaviour as per the equal's contract")
+  void testEqualsForTransitiveBehaviour_ShouldReturnTrueWhenGivenSameProduct() {
+    //Given
+    product = new Product("cornflakes", new BigDecimal("10.01"));
+    cartItem = new CartItem(product, 2);
+    CartItem secondCartItem = new CartItem(new Product("cornflakes", new BigDecimal("10.01")), 2);
+    CartItem thirdCartItem = new CartItem(new Product("cornflakes", new BigDecimal("10.01")), 2);
+
+    //When
+    boolean result = cartItem.equals(secondCartItem);
+    boolean secondResult = secondCartItem.equals(thirdCartItem);
+    boolean thirdResult = thirdCartItem.equals(cartItem);
+
+    //Then
+    Assertions.assertTrue(result);
+    Assertions.assertTrue(secondResult);
+    Assertions.assertTrue(thirdResult);
+  }
+
+  @Test
+  @DisplayName("Test hashCode should return expected hashcode on the basis of product's name & price")
+  void testHashCode_ShouldReturnExpectedHashCodeValueByProductAndQuantity() {
+    //Given
+    product = new Product("cheerios", new BigDecimal("10.01"));
+    cartItem = new CartItem(product, 2);
+    int expectedHashCode = 1467144585;
+
+    //When
+    int actualHashCode = cartItem.hashCode();
+
+    Assertions.assertEquals(expectedHashCode, actualHashCode);
+  }
+}
